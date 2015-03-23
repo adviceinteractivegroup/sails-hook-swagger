cad14101a7bf875c95f2f676768de5a3f3776dfc
Joshwa Fugett <jfugett27@gmail.com>
2015-03-23 14:08:22 -0700

    chore(missing return): Missing return part2

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
 'use strict';
 
 module.exports = function swagger(sails) {
-  defaults: {
-    __configKey__: {
-      enabled: true, actions
-    :
-      true, rest
-    :
-      true, shortcuts
-    :
-      true
-    }
-  },
-  initialize: function initialize(cb) {
-    if (!sails.config[this.configKey].enabled) {
-      sails.log.verbose('Swagger hook deactivated.');
-      return cb();
-    } else {
-      sails.log.verbose('Swagger hook activated.');
-    }
+  return {
+    defaults: {
+      __configKey__: {
+        enabled: true,
+        actions: true,
+        rest: true,
+        shortcuts: true
+      }
+    },
+    initialize: function initialize(cb) {
+      if (!sails.config[this.configKey].enabled) {
+        sails.log.verbose('Swagger hook deactivated.');
+        return cb();
+      } else {
+        sails.log.verbose('Swagger hook activated.');
+      }
 
-    console.log('Initialize Here');
-    cb();
-  },
-  routes: {
+      console.log('Initialize Here');
+      cb();
+    },
+    routes: {
 
+    }
   }
 };
